3132080b4c742a0c65daf4fdec803b4e21f68c26
NANOOS105 <heesu105@naver.com>
2024-10-14 01:54:24 +0900

    스프링 컨테이너 생성

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,11 @@
 package hello.core;
 
-import hello.core.discount.FixDiscountPolicy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,13 +14,27 @@ import hello.core.order.OrderServiceImpl;
 
 //실제 동작에 필요한 구현 객체를 생성
 //생성한 객체 인스턴스의 참조를 생성자를 통해서 주입
+@Configuration
 public class AppConfig {
+	
+	@Bean
 	public MemberService memberService() {
-		return new MemberServiceImpl(new MemoryMemberRepository());
+		return new MemberServiceImpl(memberRepository());
 		
 	}
 	
+	@Bean
+	public MemberRepository memberRepository() {
+		return new MemoryMemberRepository();
+	}
+	
+	@Bean
 	public OrderService orderService() {
-		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+		return new OrderServiceImpl(memberRepository(), discountPolicy());
+	}
+	
+	@Bean
+	public DiscountPolicy discountPolicy() {
+		return new RateDiscountPolicy();
 	}
 }
